f36bb57944472a5101512f829c8f9d9f1a7c741d
Marcus Carraschi <marcuscarraschy@gmail.com>
2022-12-30 00:57:13 -0300

    #5 : NavBar Menu - FrontEnd development

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,91 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+    <Drawer :drawer.sync="drawer"></Drawer>
+    <v-app-bar app color="deep-purple" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
     </v-app-bar>
 
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout justify-center align-center>
+          <v-flex text-xs-center>
+            <!-- <router-view></router-view> -->
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+
+    <v-footer color="indigo" app>
+      <span class="white--text">&copy; 2017</span>
+    </v-footer>
+
+    <!-- <v-main>
+      <HelloWorld />
+    </v-main> -->
+
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+//import HelloWorld from '@/components/HelloWorld';
+import Drawer from '@/components/Drawer';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    //HelloWorld,
+    Drawer
   },
 
-  data: () => ({
-    //
-  }),
+  watch: {
+    drawer: {
+      handler: function (value) {
+
+        this.$nextTick(() => {
+          try {
+            if (value) {
+              document.querySelector('.v-overlay').addEventListener('click', this.handler)
+            } else {
+              document.querySelector('.v-overlay').removeEventListener('click', this.handler)
+            }
+          } catch (e) {
+            // do nothing
+          }
+        })
+
+      }
+    }
+  },
+  methods: {
+    handler() {
+      this.drawer = false
+    }
+  },
+
+  data() {
+    return {
+      drawer: false,
+      mode: false,
+      items: [
+        {
+          route: '/',
+          title: 'Home'
+        },
+        {
+          route: '/second',
+          title: 'Second'
+        },
+        {
+          route: '/third',
+          title: 'Third'
+        },
+        {
+          route: '/fourth',
+          title: 'Fourth'
+        }
+      ]
+    }
+  }
 };
 </script>
---
+++ src/components/Drawer.vue
@@ -0,0 +1,59 @@
+<template>
+    <v-navigation-drawer v-model="drawer" absolute bottom temporary>
+        <v-list nav dense>
+            <v-list-item-group v-model="group" active-class="deep-purple--text text--accent-4">
+                <v-list-item>
+                    <v-list-item-title>Foo</v-list-item-title>
+                </v-list-item>
+
+                <v-list-item>
+                    <v-list-item-title>Bar</v-list-item-title>
+                </v-list-item>
+
+                <v-list-item>
+                    <v-list-item-title>Fizz</v-list-item-title>
+                </v-list-item>
+
+                <v-list-item>
+                    <v-list-item-title>Buzz</v-list-item-title>
+                </v-list-item>
+            </v-list-item-group>
+        </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+
+    data: () => ({
+        group: null,
+    }),
+
+    props: ['drawer'],
+
+    watch: {
+        group() {
+            this.drawer = false
+        },
+    },
+
+    computed: {
+        mode: {
+            get() {
+                return this.theme
+            },
+            set(value) {
+                this.theme = value
+            }
+        },
+        open: {
+            get() {
+                return this.drawer
+            },
+            set(value) {
+                this.$emit('drawer:update', value)
+            }
+        }
+    }
+}
+</script>
