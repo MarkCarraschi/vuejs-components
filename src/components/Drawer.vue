1c626d972aa58f1c3b3c8b85db485704a01daca9
Marcus Carraschi <marcuscarraschy@gmail.com>
2023-02-06 21:54:06 -0300

    Refactor v-list

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -2,21 +2,17 @@
     <v-navigation-drawer v-model="drawer" absolute bottom temporary>
         <v-list nav dense>
             <v-list-item-group v-model="group" active-class="deep-purple--text text--accent-4">
-                <v-list-item>
-                    <v-list-item-title>Foo</v-list-item-title>
+
+                <v-list-item v-for="item in items" :key="item.title" link>
+                    <v-list-item-icon>
+                        <v-icon>{{ item.icon }}</v-icon>
+                    </v-list-item-icon>
+
+                    <v-list-item-content>
+                        <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    </v-list-item-content>
                 </v-list-item>
 
-                <v-list-item>
-                    <v-list-item-title>Bar</v-list-item-title>
-                </v-list-item>
-
-                <v-list-item>
-                    <v-list-item-title>Fizz</v-list-item-title>
-                </v-list-item>
-
-                <v-list-item>
-                    <v-list-item-title>Buzz</v-list-item-title>
-                </v-list-item>
             </v-list-item-group>
         </v-list>
     </v-navigation-drawer>
@@ -27,6 +23,12 @@ export default {
 
     data: () => ({
         group: null,
+        items: [
+            { title: 'Home', icon: 'mdi-view-dashboard' },
+            { title: 'Account', icon: 'mdi-account-box' },
+            { title: 'Payment', icon: 'mdi-credit-card-outline' },
+            { title: 'Settings', icon: 'mdi-cog' },
+        ]
     }),
 
     props: ['drawer'],
